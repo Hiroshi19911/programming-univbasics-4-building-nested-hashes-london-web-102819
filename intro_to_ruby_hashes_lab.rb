b4f6aaa07abaf5421d198cbe003d4df4044b7372
Hiroshi Sipahioglu <hiroshis91@icloud.com>
2019-10-19 22:19:06 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,9 +1,13 @@
 def base_hash
-  hash = {:railroads => {}}
+  hash = {:railroads => {}
+    
+  }
 end
 
 def monopoly_with_second_tier
-  hash = {:railroads => {}}
+  hash = {:railroads => {}
+    
+  }
 end
 
 def monopoly_with_third_tier
